7c2b5c3fba3bde9f0d4df30d3f26fa9a0468bf71
Josh Whalen <josh_whalen@hotmail.ca>
2022-10-03 17:24:09 -0400

    Added restart logic

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,15 +2,25 @@ extends Control
 
 #onready var level1 = preload("res://Levels/Level1.tscn")
 
+onready var player = get_node("../Player")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.next_level = preload("res://Levels/Level1.tscn")
+	Global.connect("player_died", self, "_on_player_death")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _on_player_death():
+
+	get_parent().reload_current_scene()
+	#$visible = true
+	#$player.health = 100
+	#$player.position = Vector2(-400, -400)
+	#$Global.next_level = preload("res://Levels/Level1.tscn")
 
 func _on_Start_pressed():
 	
